40d4616f705b111002e950b3f81967ad86e3cb73
loongel <code@loongel.tech>
2019-03-10 08:18:03 -0400

    first commit

---
+++ create_env.sh
@@ -0,0 +1,6 @@
+conda create -n AITrading python=3.6.3 numpy=1.13.1 matplotlib ipykernel
+source activate AITrading
+pip install -r requirements.txt
+
+source activate AITrading 
+python -m ipykernel install --user --name AITrading --display-name "Python (AITrading)"
